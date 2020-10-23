7f94a63e5cf9c58d5651c9ba81adf17aa5ffb319
root <billydinh95@gmail.com>
2020-10-23 12:40:18 -0700

    index.js added

---
+++ src/validations/index.js
@@ -0,0 +1,5 @@
+module.exports.authValidation = require('./auth.validation');
+module.exports.profileValidation = require('./profile.validation');
+module.exports.userValidation = require('./user.validation');
+module.exports.jobValidation = require('./job.validation');
+module.exports.applicatinValidation = require('./application.validation');
