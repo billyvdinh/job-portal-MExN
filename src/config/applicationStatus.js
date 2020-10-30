6adbf285ce5887c454f7ca1b606c211435550940
root <billydinh95@gmail.com>
2020-10-30 16:12:19 -0700

    applicationStatus.js added

---
+++ src/config/applicationStatus.js
@@ -0,0 +1,16 @@
+const applicationStatus = {
+  PENDING: 'pending',
+  APPROVED: 'approved',
+  DISAPPROVED: 'disapproved',
+  RESUBMIT: 'resubmit',
+};
+
+const applicationActions = {
+  ATTACHMENTS: 'attachments',
+  DESCRIPTION: 'description',
+};
+
+module.exports = {
+  applicationStatus,
+  applicationActions,
+};
