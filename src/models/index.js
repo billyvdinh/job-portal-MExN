84c3adb858557a5df887b0038cfacdef5aaf1785
root <billydinh95@gmail.com>
2020-11-05 14:44:10 -0800

    index.js added

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+module.exports.Token = require('./token.model');
+module.exports.User = require('./user.model');
+module.exports.Job = require('./job.model');
+module.exports.Application = require('./application.model');
