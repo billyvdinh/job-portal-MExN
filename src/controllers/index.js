e2f04afb6fc47f624a97c9b6fb47d87a837f94d4
root <billydinh95@gmail.com>
2020-11-10 16:27:40 -0800

    index.js updated

--- src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.authController = require('./auth.controller');
-module.exports.profileController = require('./profile.controller');
-module.exports.userController = require('./user.controller');
-module.exports.jobController = require('./job.controller');
-module.exports.applicationController = require('./application.controller');
