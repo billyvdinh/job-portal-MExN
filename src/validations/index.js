9e312ee210350ba3a93c54b661b54ecd09d30ab3
root <billydinh95@gmail.com>
2020-11-16 17:43:58 -0800

    index.js updated

--- src/validations/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.profileValidation = require('./profile.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.jobValidation = require('./job.validation');
-module.exports.applicatinValidation = require('./application.validation');
