caa75cdbfaf7e57c4865061b54e98ccef76c1f7c
root <billydinh95@gmail.com>
2020-11-19 16:43:57 -0800

    index.js added

---
+++ src/routes/v1/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const profileRoute = require('./profile.route');
+const userRoute = require('./user.route');
+const jobRoute = require('./job.route');
+const applicationRoute = require('./application.route');
+const docsRoute = require('./docs.route');
+const config = require('../../config/config');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/profile',
+    route: profileRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+  {
+    path: '/jobs',
+    route: jobRoute,
+  },
+  {
+    path: '/applications',
+    route: applicationRoute,
+  },
+];
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
