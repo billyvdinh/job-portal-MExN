edb93addb5557eddb5235e0ff40edfa2a4733628
root <billydinh95@gmail.com>
2021-01-01 16:19:14 -0800

    swaggerDef.js added

---
+++ src/docs/swaggerDef.js
@@ -0,0 +1,21 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Job board API documentation',
+    version,
+    license: {
+      name: 'MIT',
+      url: 'https://jobboard-project.com/LICENSE',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${config.port}/v1`,
+    },
+  ],
+};
+
+module.exports = swaggerDef;
