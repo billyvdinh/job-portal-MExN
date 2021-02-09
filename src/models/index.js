be51314339c7dfe955848c3b364d626b1fc72e52
root <billydinh95@gmail.com>
2021-02-08 18:48:48 -0800

    index.js updated

--- src/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Job = require('./job.model');
-module.exports.Application = require('./application.model');
